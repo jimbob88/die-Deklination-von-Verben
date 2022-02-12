277edb5477ec235d283bf796be61e35908402f3b
James Blackburn <blackburnfjames@gmail.com>
2022-02-12 18:35:46 +0000

    verben_buch database

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ tree = ET.parse('dataset.xml')
 root = tree.getroot()
 print(root) 
 
+verben_buch = {}
+
 for verben_gruppe in root:
-    print(verben_gruppe.attrib)
+    print(verben_gruppe.attrib['gruppe'])
+    verben_buch[verben_gruppe.attrib['gruppe']] = {}
     for verb in verben_gruppe:
+        verben_buch[verben_gruppe.attrib['gruppe']][verb.attrib['infinitiv']] = verb
         print(verb)
+
+print(verben_buch)
