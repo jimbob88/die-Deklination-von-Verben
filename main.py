b37e31f149207406c56fafedad7b76ea2c79c3b0
James Blackburn <blackburnfjames@gmail.com>
2022-02-12 18:32:20 +0000

    Iterate through verb groups

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import xml.etree.ElementTree as ET
 
 tree = ET.parse('dataset.xml')
 root = tree.getroot()
+print(root) 
+
+for verbdatenbank in root:
+    for verben_gruppe in verbdatenbank:
+        print(verben_gruppe)
