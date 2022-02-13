e1c22fcfef28d99b096a9b6cd925838898c129ef
James Blackburn <blackburnfjames@gmail.com>
2022-02-13 09:24:57 +0000

    Formatted main.py

--- main.py
+++ main.py
@@ -7,27 +7,27 @@ from urwidtrees.decoration import ArrowTree
 import os
 
 
-tree = ET.parse('dataset.xml')
+tree = ET.parse("dataset.xml")
 root = tree.getroot()
-print(root) 
+print(root)
 
 verben_buch = {}
 
 for verben_gruppe in root:
-    print(verben_gruppe.attrib['gruppe'])
-    verben_buch[verben_gruppe.attrib['gruppe']] = {}
+    print(verben_gruppe.attrib["gruppe"])
+    verben_buch[verben_gruppe.attrib["gruppe"]] = {}
     for verb in verben_gruppe:
-        verben_buch[verben_gruppe.attrib['gruppe']][verb.attrib['infinitiv']] = verb
+        verben_buch[verben_gruppe.attrib["gruppe"]][verb.attrib["infinitiv"]] = verb
         print(verb)
 
 print(verben_buch)
 
 palette = [
-    ('body', 'black', 'light gray'),
-    ('focus', 'light gray', 'dark blue', 'standout'),
-    ('bars', 'dark blue', 'light gray', ''),
-    ('arrowtip', 'light blue', 'light gray', ''),
-    ('connectors', 'light red', 'light gray', ''),
+    ("body", "black", "light gray"),
+    ("focus", "light gray", "dark blue", "standout"),
+    ("bars", "dark blue", "light gray", ""),
+    ("arrowtip", "light blue", "light gray", ""),
+    ("connectors", "light red", "light gray", ""),
 ]
 
 # We use selectable Text widgets for our example..
@@ -35,9 +35,10 @@ palette = [
 
 class FocusableText(urwid.WidgetWrap):
     """Selectable Text used for nodes in our example"""
+
     def __init__(self, txt):
         t = urwid.Text(txt)
-        w = urwid.AttrMap(t, 'body', 'focus')
+        w = urwid.AttrMap(t, "body", "focus")
         urwid.WidgetWrap.__init__(self, w)
 
     def selectable(self):
@@ -45,13 +46,13 @@ class FocusableText(urwid.WidgetWrap):
 
     def keypress(self, size, key):
         return key
-        
-
 
 
 def unhandled_input(k):
-    #exit on q
-    if k in ['q', 'Q']: raise urwid.ExitMainLoop()
+    # exit on q
+    if k in ["q", "Q"]:
+        raise urwid.ExitMainLoop()
+
 
 print(verben_buch)
 if __name__ == "__main__":
@@ -59,16 +60,28 @@ if __name__ == "__main__":
     # stree = construct_example_tree()
     # stree = SimpleTree([[FocusableText(k), [FocusableText("generic")]] for k, v in verben_buch.items()])
     stree = SimpleTree(
-        [[FocusableText("All"), [[FocusableText(k), [[FocusableText(verb), None] for verb in v.keys()]] for k, v in verben_buch.items()]]
-         ]
+        [
+            [
+                FocusableText("All"),
+                [
+                    [
+                        FocusableText(k),
+                        [[FocusableText(verb), None] for verb in v.keys()],
+                    ]
+                    for k, v in verben_buch.items()
+                ],
+            ]
+        ]
     )
 
     # put the tree into a treebox
     treebox = TreeBox(ArrowTree(stree))
 
     # add some decoration
-    rootwidget = urwid.AttrMap(treebox, 'body')
-    #add a text footer
-    footer = urwid.AttrMap(urwid.Text('Q to quit'), 'focus')
-    #enclose all in a frame
-    urwid.MainLoop(urwid.Frame(rootwidget, footer=footer), palette, unhandled_input = unhandled_input).run() 
+    rootwidget = urwid.AttrMap(treebox, "body")
+    # add a text footer
+    footer = urwid.AttrMap(urwid.Text("Q to quit"), "focus")
+    # enclose all in a frame
+    urwid.MainLoop(
+        urwid.Frame(rootwidget, footer=footer), palette, unhandled_input=unhandled_input
+    ).run()
